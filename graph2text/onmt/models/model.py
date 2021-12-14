38f01035891d0a6c25198274acc1398d8c1a583d
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-14 15:01:29 -0800

    Tree eval

--- graph2text/onmt/models/model.py
+++ graph2text/onmt/models/model.py
@@ -2,7 +2,6 @@
 import torch
 import torch.nn as nn
 import dgl
-from onmt.decoders.dependency_tree_rel_model import pred_root
 
 
 class NMTModel(nn.Module):
@@ -182,9 +181,14 @@ class NMTPlanModel(nn.Module):
                 msp_result, etype, pred_root = self.tree_decoder.decode_mst(compat_matrix_full, root_scores)
                 # Decode the EDU order from the tree
                 dep_tree_root, new_adj_matrix = self.tree_decoder.arrange_dep_tree_rootclf(msp_result, etype, int(root))
-                uas, las = calc_uas_las(new_adj_matrix, torch.cat(adj_matrix, dim=2), pred_root, root, num_nodes)
-                print("UAS: ", uas)
+                print("ADJ MATRIX ", new_adj_matrix.shape)
+                flat_new_adj = torch.sum(new_adj_matrix, dim=2)
+                flat_gold_adj = torch.sum(torch.cat(adj_matrix, dim=2), dim=2)
+                uas = 1 - torch.sum(flat_new_adj != flat_gold_adj).float() / (num_nodes * 2) - int(root != gold_root) / num_nodes
+                las = 1 - torch.sum(new_adj_matrix != gold_adj_matrix).float() / (num_nodes * 2) - int(root != gold_root) / num_nodes                print("UAS: ", uas)
+                
                 print("LAS: ", las)
+                print("UAS: ", uas)
 
         
         dec_out, attns = self.decoder(dec_in, memory_bank,
