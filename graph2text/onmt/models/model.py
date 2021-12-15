63b1a5f5a0cf3175dd49635ac7468a44a5dd040d
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-14 22:20:06 -0800

    UAS, LAS

--- graph2text/onmt/models/model.py
+++ graph2text/onmt/models/model.py
@@ -176,8 +176,8 @@ class NMTPlanModel(nn.Module):
                 tree_loss += self.tree_decoder(edus, tree_graph, adj_matrix, root, rels)
                 print(root)
                 print("edus", edus.shape,edus)
-                compat_matrix_full = self.tree_decoder.get_compat_matrix(edus)
-                root_scores = self.tree_decoder.root_clf(edus.squeeze(0)).view(-1)
+                compat_matrix_full = self.tree_decoder.get_compat_matrix(edus.squeeze(0))
+                root_scores = self.tree_decoder.root_clf(edus).view(-1)
                 print("scores ", root_scores.shape, root_scores)
                 # Decode the tree structure
                 msp_result, etype, pred_root = self.tree_decoder.decode_mst(compat_matrix_full, root_scores)
