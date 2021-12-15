f1514bfe698d9818a6f809504aaea60759935543
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-15 02:34:09 -0800

    Generator

--- graph2text/onmt/models/model.py
+++ graph2text/onmt/models/model.py
@@ -129,6 +129,8 @@ class NMTPlanModel(nn.Module):
         plan_dec_out, plan_attns = self.plan_decoder(plan, memory_bank,
                                       memory_lengths=src_lengths,
                                       with_align=with_align)
+        print("Generator? ", self.plan_generator)
+        
         tree_loss = 0
         num_trees = len(trees)
         if self.tree_decoder:
@@ -189,11 +191,11 @@ class NMTPlanModel(nn.Module):
                 print("UAS: ", uas.item())
                 print("LAS: ", las.item())
                 
-                pred_order = self.tree_decoder.node_order((edus.unsqueeze(0), torch.mean(edus, dim=0).unsqueeze(0)), dep_tree_root)
-                print("PRED ORDER", pred_order)
-                # pred_order = torch.tensor(pred_order, device=self.config[DEVICE])      
-                # Make 1 to num_nodes instead of 0 to num_nodes - 1
-                pred_order += 1
+                # pred_order = self.tree_decoder.node_order((edus.unsqueeze(0), torch.mean(edus, dim=0).unsqueeze(0)), dep_tree_root)
+                # print("PRED ORDER", pred_order)
+                # # pred_order = torch.tensor(pred_order, device=self.config[DEVICE])      
+                # # Make 1 to num_nodes instead of 0 to num_nodes - 1
+                # pred_order += 1
 
         
         dec_out, attns = self.decoder(dec_in, memory_bank,
