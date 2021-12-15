b8638f5d165bd13d73e6cf21cedbf7d805810f4c
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-15 13:08:25 -0800

    Generator

--- graph2text/onmt/models/model.py
+++ graph2text/onmt/models/model.py
@@ -199,10 +199,9 @@ class NMTPlanModel(nn.Module):
                 print("LAS: ", las.item())
                 
                 # (1, num tokens, 512), (1, 1, 512)
-                edus = edus.squeeze(0)
                 print("EDUS ", edus.shape)
-                print("doc embed ", torch.mean(edus, dim=0).shape)
-                pred_order = self.tree_decoder.node_order((edus.unsqueeze(0), torch.mean(edus, dim=0).unsqueeze(0)), dep_tree_root)
+                print("doc embed ", torch.mean(edus, dim=1).shape)
+                pred_order = self.tree_decoder.node_order((edus, torch.mean(edus, dim=1)), dep_tree_root)
                 print("PRED ORDER", pred_order)
                 pred_order = torch.tensor(pred_order, device=self.config[DEVICE])      
                 # Make 1 to num_nodes instead of 0 to num_nodes - 1
