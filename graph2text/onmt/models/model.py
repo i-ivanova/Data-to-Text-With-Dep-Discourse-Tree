693bd40e96030dd0a0abddb496bc38072acb9f5c
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-14 22:54:25 -0800

    Fix

--- graph2text/onmt/models/model.py
+++ graph2text/onmt/models/model.py
@@ -186,7 +186,7 @@ class NMTPlanModel(nn.Module):
                 dep_tree_root, new_adj_matrix = self.tree_decoder.arrange_dep_tree_rootclf(msp_result, etype, int(pred_root))
                 print("ADJ MATRIX ", new_adj_matrix.shape)
                 flat_new_adj = torch.sum(new_adj_matrix, dim=2)
-                flat_gold_adj = torch.sum(torch.cat(adj_matrix, dim=2), dim=2)
+                flat_gold_adj = torch.sum(torch.cat([adj_matrix[0].squeeze(0), adj_matrix[1].squeeze(0)], dim=2), dim=2)
                 print("ADJ MATRIX1 ", flat_new_adj.shape)
                 print("ADJ MATRIX2 ", flat_gold_adj.shape, adj_matrix[0].shape, adj_matrix[0].shape)                
                 uas = 1 - torch.sum(flat_new_adj != flat_gold_adj).float() / (num_nodes * 2) - int(pred_root != root) / num_nodes
