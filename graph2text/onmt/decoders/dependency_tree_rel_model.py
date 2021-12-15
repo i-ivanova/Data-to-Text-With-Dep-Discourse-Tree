0f31052a74685a741dde7eafb76cb5366b4422d6
i-ivanova <inna.ivanova.vr@gmail.com>
2021-12-14 22:41:26 -0800

    Fix

--- graph2text/onmt/decoders/dependency_tree_rel_model.py
+++ graph2text/onmt/decoders/dependency_tree_rel_model.py
@@ -240,8 +240,9 @@ class DependencyTreeRelModel(nn.Module):
         beam_size = min(num_nodes, 5)
         arcs = []
         
-        compat_matrix, etype = th.max(compat_matrix_full, dim=2)
+        compat_matrix, etype = th.max(compat_matrix_full.unsqueeze(0), dim=2)
         print("COMPAT ", compat_matrix.shape, compat_matrix[0, 0], compat_matrix)
+        print("ETYPE: ", etype)
         
         _, indices = th.topk(root_scores, beam_size)
         
